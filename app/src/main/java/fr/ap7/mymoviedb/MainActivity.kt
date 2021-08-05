c95ec8d38e2887218616a2129212aa2b87e43084
ap7 <arhouston7@gmail.com>
2021-08-06 01:27:08 +0200

    Data Binding - Add data binding dependencies in build.gradle - Create RecyclerViewBinding.kt - Updates activity_main.xml : define the data element for the data to be bound - Updates MainActivity.kt : remove the setcontentview for the inflate method of the binding class

--- app/src/main/java/fr/ap7/mymoviedb/MainActivity.kt
+++ app/src/main/java/fr/ap7/mymoviedb/MainActivity.kt
@@ -4,15 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
+import fr.ap7.mymoviedb.databinding.ActivityMainBinding
 import fr.ap7.mymoviedb.model.Movie
-import java.util.Calendar
-import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
-
     private val movieAdapter by lazy {
         MovieAdapter(object : MovieAdapter.MovieClickListener {
             override fun onMovieClick(movie: Movie) {
@@ -23,34 +22,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val recyclerView: RecyclerView = findViewById(R.id.movie_list)
         recyclerView.adapter = movieAdapter
 
         val movieRepository = (application as MovieApplication).movieRepository
-
         val movieViewModel = ViewModelProvider(this, object : ViewModelProvider.Factory {
             override fun <T : ViewModel?> create(modelClass: Class<T>): T {
                 return MovieViewModel(movieRepository) as T
             }
         }).get(MovieViewModel::class.java)
 
-        movieViewModel.popularMovies.observe(this, { popularMovies ->
-            movieAdapter.addMovies(popularMovies
-                // filter function to only include movies released
-                // this year.
-                .filter {
-                    it.release_date.startsWith(
-                        Calendar.getInstance().get(Calendar.YEAR).toString()
-                    )
-                }
-                // sorted by title using Kotlin's sortedBy function.
-                .sortedBy { it.title }
-                // make uppercase the title of movie using map
-                .map { it.copy(title = it.title.toUpperCase(Locale.getDefault())) }
-            )
-        })
+        binding.viewModel = movieViewModel
+        binding.lifecycleOwner = this
+
         movieViewModel.error.observe(this, { error ->
             Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
         })
